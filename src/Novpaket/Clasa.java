7c13acfde10d1f316fe588ae751a28194d5c0d20
DanielaDanaj <dani_mirkovska@live.com>
2023-05-18 18:49:59 +0200

    My first commit

---
+++ src/Novpaket/Clasa.java
@@ -0,0 +1,15 @@
+package Novpaket;
+
+public class Clasa {
+    public static void main(String[] args) {
+        System.out.println("Hello");}
+
+/*
+
+ */
+public static void sayHello(String name){
+    //TODO:Add support for multiple users
+        System.out.println("Hello +name");
+    }
+
+    }
---
+++ src/Novpaket/Interface.java
@@ -0,0 +1,4 @@
+package Novpaket;
+
+public interface Interface {
+}
